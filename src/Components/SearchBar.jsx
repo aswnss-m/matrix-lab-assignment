d7e33e0075eb791e0aa63005e4791ce375d42585
aswnss_ubuntu <aswinlalsct@gmail.com>
2023-08-27 13:01:45 +0530

    styling done

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import "./SearchBar.css";
 
 function SearchBar() {
+    // set the default value of the input
+    const [input, setInput] = useState('Search');
+    
   return (
-    <div className='searchBar'>SearchBar</div>
+    <div className='searchBar'>
+            <div className="searchInput">
+                <input type="text" placeholder="Search" onChange={(e)=>{
+                    setInput(e.target.value);
+                }} value={input}/>
+                <button type='submit'>
+                    <span className='material-symbols-outlined'>search</span>
+                </button>
+            </div>
+            <button className='walletButton'>Connect</button>
+    </div>
   )
 }
 
