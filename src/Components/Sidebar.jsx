4867a223df713eb2da565d86a592e10d190ed30e
aswnss_ubuntu <aswinlalsct@gmail.com>
2023-08-27 10:54:45 +0530

    sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import "./Sidebar.css";
+import icon from "../Assets/icons/Vector-5.png";
+import logo from "../Assets/icons/NFTify.png";
+import token_icon from "../Assets/icons/Vector-1.png";
+import pair_icon from  "../Assets/icons/Vector.png"
+import fb from "../Assets/icons/facebook.png";
+import twitter from "../Assets/icons/twitter.png";
+import linkedin from "../Assets/icons/linkedin.png";
 
 function Sidebar() {
   return (
     <div className="Sidebar">
-      
+      <div className="logo">
+        <img src={icon} alt="image-icon" />
+        <img src={logo} alt="NFTify logo" />
+      </div>
+      <ul className="menu">
+        <li className="menuItem">
+          <img src={token_icon} alt="Token Address icon" />
+          <span>Token Address</span>
+        </li>
+        <li className="menuItem active">
+            <img src={pair_icon} alt="Pair Address icon" />
+            <span>Pair Address</span>
+        </li>
+      </ul>
+      <div className="socials">
+        <img src={fb} alt="facebook icon" />
+        <img src={linkedin} alt="linkedin icon" />
+        <img src={twitter} alt="twitter icon" />
+      </div>
     </div>
   )
 }
